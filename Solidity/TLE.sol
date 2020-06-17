f34e570b18c0b86648e7709af291946eff06fe0c
ChristianDahdah <christiandahdah@gmail.com>
2020-06-17 13:13:27 +0200

    Adding TLE.js and TLE.sol

---
+++ Solidity/TLE.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity >=0.5.0 <0.7.0;
+
+import "./Provider_Depreciation_Contract.sol";
+
+contract TLE_Contract is Provider_Depreciation_Contract {
+    
+}
