ddc39cfc5b6d8b24e01ef1484d2c31c66b3c2e4a
Kheil-Z <ziadishappy@gmail.com>
2020-06-17 08:19:34 +0200

    Changed contract and tests for bytes16 test

--- Solidity/StringBytesTest.sol
+++ Solidity/StringBytesTest.sol
@@ -5,13 +5,15 @@ pragma solidity >=0.5.0 <0.7.0;
 
 contract StringBytesTest {
     string public testString;
+    bytes16 public byt;
 
-    event Event(string indexed testStringIndexed, string testString);
+    event Eventee(string indexed testStringIndexed, string testString, bytes16 byt);
 
-    function set( string memory _testString) public {
+    function set( string memory _testString, bytes16 _byt) public {
         testString = _testString;
+        byt = _byt;
 
-        emit Event(_testString, _testString);
+        emit Eventee(_testString, _testString, byt);
     }
 
 }
